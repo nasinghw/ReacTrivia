bf1e74720740d55ca1abda016d1963f4662bb78f
rober-web <robp.medeiros@gmail.com>
2024-03-14 15:19:24 +0000

    Created content to QuestionsPage component

---
+++ src/pages/QuestionsPage.jsx
@@ -0,0 +1,13 @@
+import QuestionContent from '../components/questionsPage/QuestionContent';
+
+const QuestionsPage = () =>{
+
+    return(
+        <>
+            <QuestionContent />
+        </>
+    );
+
+}
+
+export default QuestionsPage;
