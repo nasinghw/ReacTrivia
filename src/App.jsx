15067e3515c484f3cc6d4874cc02b0fa054b1513
Rfn <arfin@hotmail.co.uk>
2024-03-15 19:39:02 +0000

    landing page floating particles added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import LandingPage from './pages/LandingPage.jsx'
 
+const questionArray = [
+];
+
+
+const apiUrl = 'https://the-trivia-api.com/v2/questions?limit=10&categories=science,film_and_tv';
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(userData => {
+    for (let index = 0; index < userData.length; index++) {
+      const { category, correctAnswer, incorrectAnswers, question } = userData[index]    
+      const questionInfo = {
+        category: category,
+        correctAnswer: correctAnswer,
+        incorrectAnswers: incorrectAnswers,
+        question: question
+      }
+      questionArray.push( questionInfo )
+
+    }
+
+    console.log(questionArray)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
 function App() {
 
   return (
---
+++ src/utils/API.jsx
@@ -0,0 +1,110 @@
+import '../App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [tdbQuestion, settdbQuestion] = useState([]);
+  const [rightAnswer, setrightAnswer] = useState("");
+  const [startPoints, setstartPoints] = useState(0);
+  const [allPossibleAnswers, setAllPossibleAnswers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //combines correct and incorrect answer into single array
+  async function combineAllAnswers(inrightAnswers, rightAnswer) {
+    let allAnswers = [];
+    inrightAnswers.map((item) => {
+      item.incorrect_answers.map((inrightAnswer) => {
+        allAnswers.push(inrightAnswer)
+      });
+    });
+    allAnswers.push(rightAnswer);
+    //Randomize order of answers in array
+    allAnswers.sort(() => Math.random() - 0.5);
+    setAllPossibleAnswers(allAnswers);
+  }
+
+  //Make api call to trivia api
+  async function getTriviaData() {
+    //Set loading boolean to true so that we know to show loading text
+    setLoading(true);
+
+    //Make trivia api call using axios
+    const resp = await axios.get("https://opentdb.com/api.php?amount=1");
+
+    settdbQuestion(resp.data.results);
+    setrightAnswer(resp.data.results[0].correct_answer);
+
+    //Combines correct and incorrect answers into single array
+    await combineAllAnswers(resp.data.results, resp.data.results[0].correct_answer);
+
+
+    //console logs
+    console.log("Trivia Question:", tdbQuestion);
+    console.log("Correct Answer:", rightAnswer);
+    console.log("All Possible Answers:", allPossibleAnswers);
+    console.log("correct Answer", rightAnswer);
+    console.log("All Answers", allPossibleAnswers);
+
+
+
+    //Set loading boolean to false so that we know to show trivia question
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getTriviaData();
+  }, []);
+
+  function verifyAnswer(selectedAnswer) {
+    //If the selected answer equals the correct answer, then we get the next trivia quesiton and increase the current points by 1
+    if (selectedAnswer === rightAnswer) {
+      getTriviaData();
+      setstartPoints(startPoints + 1);
+    } else {
+      //If the selected answer does not equal the correct answer, decreaes the current points by 1
+      setstartPoints(startPoints - 1);
+    }
+  }
+
+  //Converts html code to regular characters
+  function removeCharacters(question) {
+    return question.replace(/(&quot\;)/g, "\"").replace(/(&rsquo\;)/g, "\"").replace(/(&#039\;)/g, "\'").replace(/(&amp\;)/g, "\"");
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {loading ? "Trivia Question Loading..." : <div>
+          <div>
+            Current Points: {startPoints}
+          </div>
+          <br />
+
+          {tdbQuestion.map((triviaData, index) =>
+            <div key={index}>
+              <div>
+                {removeCharacters(triviaData.question)}
+              </div>
+              <br />
+              <div>
+                {
+                  allPossibleAnswers.map((answer, index) =>
+                    <div key={index}>
+                      <button key={index} onClick={() => verifyAnswer(answer)} >
+                        {removeCharacters(answer)}
+                      </button>
+                    </div>
+                  )
+                }
+              </div>
+            </div>
+          )}
+        </div>
+        }
+      </header>
+    </div>
+  );
+}
+
+export default App;
