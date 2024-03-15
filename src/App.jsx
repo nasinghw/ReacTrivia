ac465dd1ea26a1bc48db3585738f657db319e48c
Pazoraf <alexander.flaherty@hotmail.com>
2024-03-15 19:26:40 +0000

    Added question json file while trying to fix fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
-const questionArray = [
-];
-
-
-const apiUrl = 'https://the-trivia-api.com/v2/questions?limit=10&categories=science,film_and_tv';
-
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(userData => {
-    for (let index = 0; index < userData.length; index++) {
-      const { category, correctAnswer, incorrectAnswers, question } = userData[index]    
-      const questionInfo = {
-        category: category,
-        correctAnswer: correctAnswer,
-        incorrectAnswers: incorrectAnswers,
-        question: question
-      }
-      questionArray.push( questionInfo )
-
-    }
-
-    console.log(questionArray)
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+import QuestionContent from './components/questionsPage/QuestionContent'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <QuestionContent />
   )
 }
 
--- src/components/questionsPage/QuestionContent.jsx
+++ src/components/questionsPage/QuestionContent.jsx
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import questions from '../../data/questions.json'
+
 
 const questionArray = [];
 
+// const apiUrl = 'https://the-trivia-api.com/v2/questions?limit=10&categories=science,film_and_tv';
 
-const apiUrl = 'https://the-trivia-api.com/v2/questions?limit=10&categories=science,film_and_tv';
+// fetch(questions)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(userData => {
+//     // console.log(userData)
+//     for (let index = 0; index < userData.length; index++) {
+//       const { category, correctAnswer, incorrectAnswers, question } = userData[index]    
+//       const questionInfo = {
+//         category: category,
+//         correctAnswer: correctAnswer,
+//         choices: incorrectAnswers,
+//         question: question.text
+//       }
+//       questionInfo.choices.push(correctAnswer)
+    
+//       questionArray.push( questionInfo )
 
-// Make a GET request using the Fetch API
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+//     }
+
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+// ;
+
+
+    for (let index = 0; index < questions.length; index++) {
+      const { category, correctAnswer, incorrectAnswers, question } = questions[index]    
+      const questionInfo = {
+        category: category,
+        correctAnswer: correctAnswer,
+        choices: incorrectAnswers,
+        question: question.text
+      }
+      questionInfo.choices.push(correctAnswer)
+    
+      questionArray.push( questionInfo )
+
     }
-    return response.json();
-  })
-  .then(userData => {
-
-    userData.forEach((index) => {
-        questionArray.push(index.category)
-    })
-
-    console.log(userData)
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
 
 const QuestionContent = ()=>{
-
+  console.log(questionArray)
     return(
         <>
         <h1>This is the Question Content Component</h1>
+        <h2>{questionArray[0].question}</h2>
         </>
     )
 
