daf680fa65d62cc20fdc4f8196f1eb341a835e9e
Tom Airey <aireytf19@gmail.com>
2024-03-18 10:03:38 +0000

    add game summary js content to results page comp

---
+++ src/components/resultsPage/GameSummary.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GameSummary = () => {
+
+  return (
+    <div className="game-summary">
+      <h2>Game Summary</h2>
+      <ul>
+        <li>Score: {score} out of {}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default GameSummary;
