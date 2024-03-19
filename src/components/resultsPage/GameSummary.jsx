f3c7ff98bb73240f7ea5908d66f794f8500dbe24
Pazoraf <alexander.flaherty@hotmail.com>
2024-03-19 20:02:35 +0000

    Added local storage saving for results

--- src/components/resultsPage/GameSummary.jsx
+++ src/components/resultsPage/GameSummary.jsx
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom';
 
 const GameSummary = ({ questionArray, result }) => {
   const { score, correct, incorrect } = result
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const userName = event.target.userName.value;
+    localStorage.setItem("userName", userName)
+    localStorage.setItem("userScore", score)
+  }
+  
   return (
     <div className="game-summary">
       <h2>Game Summary</h2>
@@ -11,6 +19,10 @@ const GameSummary = ({ questionArray, result }) => {
         <li>Correct Answers: {correct} out of {questionArray.length}</li>
         <li>Incorrect Answers: {incorrect} out of {questionArray.length}</li>
       </ul>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" name ="userName" placeholder='Your Name Here'></input>
+        <button className="btn-primary" type="submit">Submit!</button>
+      </form>
       <Link to="/">
         <button className="btn-primary" >Go home</button>
       </Link>
