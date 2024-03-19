40a60c73d9da319b6b3918b871648afe6a356774
Pazoraf <alexander.flaherty@hotmail.com>
2024-03-19 14:23:28 +0000

    Added basic for to user form page

--- src/components/userFormPage/AdvancedForm.jsx
+++ src/components/userFormPage/AdvancedForm.jsx
@@ -43,6 +43,50 @@ import React, { useEffect, useState } from "react";
 
     return(
         <>
+        <form class="w-full max-w-lg">
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <div class="relative">
+                        <div>
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                            Amount of questions
+                        </label>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="# of questions (max 50)"/>
+                        </div>
+                        <div>
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                            Difficulty
+                        </label>
+                            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <option>Easy</option>
+                                <option>Medium</option>
+                                <option>Hard</option>
+                            </select>
+                        </div>
+                        <div>
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                            Category
+                        </label>
+                            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <option>music</option>
+                                <option>sport_and_leisure</option>
+                                <option>film_and_tv</option>
+                                <option>arts_and_literature</option>
+                                <option>history</option>
+                                <option>society_and_culture</option>
+                                <option>science</option>
+                                <option>geography</option>
+                                <option>food_and_drink</option>
+                                <option>general_knowledge</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </form>
         </>
     );
 
