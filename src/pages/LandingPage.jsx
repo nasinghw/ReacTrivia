047bc605a1a871d944ca117344c633544ad77148
Rfn <arfin@hotmail.co.uk>
2024-03-19 20:09:32 +0000

    cleaned up directory and added floatingparticles to all pages

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react'; 
 import Description from '../components/landingPage/Description';
-import FloatingIcons from '../components/landingPage/FloatingIcons';
 import { Link } from 'react-router-dom';
+// import FloatingIcons from '../template/FloatingIcons';
 // import Button from '../template/Button';
 // import Header from '../template/Header';
 // import Footer from '../template/Footer';
-// import FloatingIcons from '../components/landingPage/FloatingIcons';
 
 const LandingPage = ({ setResult }) => {
 
@@ -19,10 +18,8 @@ const LandingPage = ({ setResult }) => {
 
     return (
         <>
-            <FloatingIcons />
+           
             <Description />
-            {/* Temporary Placeholder Buttons */}
-            {/* TODO: Link Template Buttons once ready */}
             <div className='btn-container'>
                 <Link to="/user-form">
                     <button className="btn-primary" onClick={resetResults}>Start a Quiz</button>
