b7c30ac14e88a7a9a799f3d1ac7ffbf58d2f6d29
rober-web <robp.medeiros@gmail.com>
2024-03-20 00:48:42 +0000

    styling landing page buttons and set icons

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'; 
-import Description from '../components/landingPage/Description';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import Description from "../components/landingPage/Description";
+import { Link } from "react-router-dom";
 // import FloatingIcons from '../template/FloatingIcons';
 // import Button from '../template/Button';
 // import Header from '../template/Header';
@@ -15,20 +15,56 @@ const LandingPage = ({ setResult }) => {
     });
   };
 
-    return (
-        <>
-           
-            <Description />
-            <div className='btn-container'>
-                <Link to="/user-form">
-                    <button className="btn-primary" onClick={resetResults}>Start a Quiz</button>
-                </Link>
-                <Link to="/highscores">
-                    <button>Highscores</button>
-                </Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Description />
+      <div className="btn-container flex justify-center mt-3">
+        <Link
+          className="btn-primary btn-primary flex items-center justify-center rounded-full py-2 px-4 mx-2 hover:bg-yellow-300 w-40 "
+          to="/user-form"
+        >
+          <button
+            onClick={resetResults}
+            className="flex items-center justify-center"
+          >
+            <span class="mr-2">Start a Quiz</span>{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </Link>
+        <Link
+          className="btn-primary btn-primary btn-primary flex items-center justify-center rounded-full py-2 px-4 mx-2 hover:bg-yellow-300 w-40"
+          to="/highscores"
+        >
+          <button className="flex items-center justify-center">
+            <span class="mr-2 flex">Highscores</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
