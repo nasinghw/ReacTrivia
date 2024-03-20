5a390883219093905ce88674fa43a5bbb3c8cba4
Rfn <arfin@hotmail.co.uk>
2024-03-20 22:30:01 +0000

    adjusted timing for landing page typed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -21,7 +21,7 @@ const LandingPage = ({ setResult }) => {
       startWhenVisible
       strings={["Welcome to <strong>&lt;ReacTrivia /&gt;"]}
       typeSpeed={45}
-      startDelay={2000}
+      startDelay={1000}
       cursorChar="_"
      />
      </div>
