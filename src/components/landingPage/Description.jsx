4dcfe6ce71ab2f52f531cfeb6f214f2bcd8a3637
Rfn <arfin@hotmail.co.uk>
2024-03-20 22:05:55 +0000

    added animated logo to landing page

--- src/components/landingPage/Description.jsx
+++ src/components/landingPage/Description.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { ReactTyped } from "react-typed";
 
 const Description = () => {
   return (
     <div>
-      <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl mt-20">
-        Welcome to {"<ReacTrivia />"}
-      </h1>
-      <p className="text-base md:text-md lg:text-lg xl:text-1xl mt-20 p-3">
+      <div className="my-20 text-center text-6xl 	" >
+ <ReactTyped
+   startWhenVisible
+   strings={["Welcome to <strong>&lt;ReacTrivia /&gt;"]}
+   typeSpeed={45}
+   startDelay={2000}
+   cursorChar="_"
+  />
+  </div>
+
+      <p className="text-base md:text-md lg:text-lg xl:text-1xl text-center mt-20 p-3 mx-5">
         Test your knowledge and skills by playing through our quizzes, custom
         built just for you! ReacTrivia allows you to choose from a variety of
         categories to find out how far your knowledge can take you. Battle it
         out with friends to see who can top the leaderboard with their highscore, and become the
         ultimate quiz master.
+        <br /> 
         <br /> 
         Traverse into a world of trivia, using the button below...{" "}
       </p>
--- src/template/Header.jsx
+++ src/template/Header.jsx
@@ -1,2 +1,24 @@
 import React from "react";
 import { ReactTyped } from "react-typed";
+
+const Header = () => {
+    
+    return(
+    <>
+    <div className="" id="header"></div>
+        <div className="my-20 text-center text-7xl 	" >
+        <ReactTyped
+   
+            strings={["&lt;ReacTrivia /&gt;"]}
+            typeSpeed={45}
+            backSpeed={45}
+            backDelay={2000}
+            cursorChar="_"
+            loop
+            loopCount={5}
+        />
+
+        </div>
+    </>
+  )};
+export default Header;
