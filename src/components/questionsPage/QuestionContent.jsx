d9938362bf09fdaa96f1212ff61f14f5fb8f3c97
rober-web <robp.medeiros@gmail.com>
2024-03-21 01:58:47 +0000

    adjusted viewport size of buttons on small devices on the question page

--- src/components/questionsPage/QuestionContent.jsx
+++ src/components/questionsPage/QuestionContent.jsx
@@ -102,25 +102,25 @@ const QuestionContent = ({
       </ul>
       <button
         onClick={nextQuestion}
-        className="bg-green-500 hover:bg-green-600 text-white py-2 px-8 rounded mt-5 mb-10 w-full md:w-auto"
+        className="justify-centerbg-green-500 hover:bg-green-600 text-white py-2 px-8 rounded mt-5 mb-10 w-full md:w-auto"
       >
         {/*Clicking button next question to change questions forward */}
         {questionCount === questionArray.length - 1 ? "Finish" : "Next"}
       </button>
-      <div className="mt-5">
-        <button className="inline-flex items-center px-3 py-1 text-sm font-medium text-center text-white bg-orange-700 rounded-lg focus:outline-none  dark:bg-orange-600 ">
+      <div className="mt-5 flex flex-col gap-2 justify-center md:flex-row sm:gap-4 md:gap-2">
+        <button className="justify-center inline-flex items-center px-3 py-1 text-sm font-medium text-center text-white bg-orange-700 rounded-lg focus:outline-none  dark:bg-orange-600 ">
           Score:
           <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-gray-800 bg-orange-200 rounded-full">
             {score}
           </span>
         </button>{" "}
-        <button className="inline-flex items-center px-3 py-1  text-sm font-medium text-center text-white bg-green-700 rounded-lg focus:outline-none  dark:bg-green-600 ">
+        <button className="justify-center inline-flex items-center px-3 py-1  text-sm font-medium text-center text-white bg-green-700 rounded-lg focus:outline-none  dark:bg-green-600 ">
           Correct:
           <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-gray-800 bg-green-200 rounded-full">
             {correct}
           </span>
         </button>{" "}
-        <button className="inline-flex items-center px-3 py-1  text-sm font-medium text-center text-white bg-red-700 rounded-lg focus:outline-none  dark:bg-red-600 ">
+        <button className="justify-center inline-flex items-center px-3 py-1  text-sm font-medium text-center text-white bg-red-700 rounded-lg focus:outline-none  dark:bg-red-600 ">
           Incorrect:
           <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-gray-800 bg-red-200 rounded-full">
             {incorrect}
@@ -129,7 +129,7 @@ const QuestionContent = ({
       </div>
       <div className="mt-5">
         <button
-          className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-amber-600 hover:bg-amber-500 rounded focus:outline-none  dark:bg-orange-600 "
+          className="w-full md:w-auto inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-center text-white bg-amber-600 hover:bg-amber-500 rounded focus:outline-none  dark:bg-orange-600 "
           onClick={handleQuitQuiz}
         >
           QUIT QUIZ
